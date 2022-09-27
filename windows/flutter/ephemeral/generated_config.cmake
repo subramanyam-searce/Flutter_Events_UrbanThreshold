b413546f714b87f602b7df2cafb7146865eacec4
Subramanyam-Searce <114482906+Subramanyam-Searce@users.noreply.github.com>
2022-09-27 16:18:35 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Development\\sdks\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Development\\flutter\\smart_visiting_card_1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Development\\sdks\\flutter"
+  "PROJECT_DIR=C:\\Development\\flutter\\smart_visiting_card_1"
+  "FLUTTER_ROOT=C:\\Development\\sdks\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Development\\flutter\\smart_visiting_card_1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Development\\flutter\\smart_visiting_card_1"
+  "FLUTTER_TARGET=C:\\Development\\flutter\\smart_visiting_card_1\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Development\\flutter\\smart_visiting_card_1\\.dart_tool\\package_config.json"
+)
